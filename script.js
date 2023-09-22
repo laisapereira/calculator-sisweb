38156828d628161c1edea3e8efb81dcb4d086070
Laisa Pereira <laisasilva.pereira@yahoo.com>
2023-09-21 23:39:56 -0300

    fatando apenas adicionar histórico das operações e estilização

--- script.js
+++ script.js
@@ -33,6 +33,46 @@ let currentInput = "";
     }
   }
 
+  function fatorial() {
+    const result = parseInt(resultElement.innerHTML)
+    if (isNaN(result)) {
+      alert("Por favor, insira um número válido antes de calcular o fatorial.");
+      return;
+    }
+  
+    if (result < 0) {
+      alert("O fatorial não pode ser feito para números negativos.");
+      return;
+    }
+  
+    if (result === 0 || result === 1) {
+      resultElement.innerHTML = 1;
+    } else {
+      let newResult = 1;
+      for (let i = 2; i <= result; i++) {
+        newResult *= i;
+      }
+      resultElement.innerHTML = newResult;
+    }
+  }
+
+  function sqrtNumber() {
+    const result = parseInt(resultElement.innerHTML)
+    if (isNaN(result)) {
+      alert("Por favor, insira um número válido antes de calcular a raiz quadrada.");
+      return;
+    }
+  
+    if (result < 0) {
+      alert("A raiz quadrada não pode ser feita para números negativos.");
+      return;
+    }
+  
+    resultElement.innerHTML = Math.sqrt(result);
+
+  }
+
+
   function handleButtonClick(event) {
     switch (true) {
       case event.target.classList.contains("clean-button"):
@@ -43,6 +83,12 @@ let currentInput = "";
         break;
       case event.target.classList.contains("btn"):
         handleNumberClick(event);
+        break;
+      case event.target.classList.contains("fatorial"):
+        fatorial();
+        break;
+      case event.target.classList.contains("sqrt"):
+        sqrtNumber();
         break;
       default:
         calculate(); 
