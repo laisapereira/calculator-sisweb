02752b794d4d624bfec8d392b71859ab878dc839
Laisa Pereira <laisasilva.pereira@yahoo.com>
2023-09-21 22:41:31 -0300

    requisitos obrigatorios estao funcionais, falta estilizaçao

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 
 
-// DOMContentLoaded:
-// codigo seja executado somente após o carregamento completo do DOM
+// DOMContentLoaded: codigo seja executado somente após o carregamento completo do DOM
 
 document.addEventListener("DOMContentLoaded", function () {
 const calculatorDiv = document.getElementById("calculator");
@@ -9,14 +8,47 @@ const resultElement = document.getElementById("result");
 let currentInput = "";
 
 
-        function handleButtonClick(event) {
-        if (event.target.classList.contains("btn")) {
-          const pValue = event.target.value.trim();
-          currentInput += pValue;
-          resultElement.innerHTML = currentInput;
-        }
-      }
-      calculatorDiv.addEventListener("click", handleButtonClick);
+  function clearAll() {
+    currentInput = "";
+    resultElement.innerHTML = "0";
+  }
 
-      
-    })
+  function clearLastButton() {
+    const result = resultElement.innerHTML;
+    resultElement.innerHTML = result.substring(0, result.length - 1);
+    currentInput = resultElement.innerHTML;
+  }
+
+  function handleNumberClick(event) {
+    const pValue = event.target.value;
+    currentInput += pValue;
+    resultElement.innerHTML = currentInput;
+  }
+
+  function calculate() {
+    var result = resultElement.innerHTML;
+    if(result) {
+      resultElement.innerHTML = eval(result);
+      currentInput = resultElement.innerHTML;
+    }
+  }
+
+  function handleButtonClick(event) {
+    switch (true) {
+      case event.target.classList.contains("clean-button"):
+        clearAll();
+        break;
+      case event.target.classList.contains("clean-each-button"):
+        clearLastButton();
+        break;
+      case event.target.classList.contains("btn"):
+        handleNumberClick(event);
+        break;
+      default:
+        calculate(); 
+        break;
+    }
+  }
+
+  calculatorDiv.addEventListener("click", handleButtonClick);
+});
